5b391ce891d081bc9ccb7e12e7ca92de61122373
dridanasiba <nasiba.mabetova11@gmail.com>
2025-04-20 17:46:19 +0600

    Lesson3-Homework2

--- src/main.py
+++ src/main.py
@@ -1,54 +1,30 @@
-
 import flet as ft
 
 def main(page: ft.Page):
-    expenses = []  
-    expenses_list_view = ft.Column()  
-    total_amount = ft.Text(value="Общая сумма: 0", weight=ft.FontWeight.BOLD)
+    friends = []  
 
-    expense_name_input = ft.TextField(label="Название расхода")
-    expense_amount_input = ft.TextField(label="Сумма расхода")
+    first_name_input = ft.TextField(label="Имя друга")
+    last_name_or_age_input = ft.TextField(label="Фамилия/Возраст")
 
-    def add_expense(e):
-        name = expense_name_input.value.strip()
-        amount_str = expense_amount_input.value.strip()
+    def add_friend(e):
+        first_name = first_name_input.value.strip()
+        last_name_or_age = last_name_or_age_input.value.strip()
 
-        if name and amount_str.isdigit():
-            amount = int(amount_str)
-            expense = {"название": name, "сумма": amount}
-            expenses.append(expense)
-
-            
-            expenses_list_view.controls.append(ft.Text(f"{expense['название']}: {expense['сумма']} сом"))
-
-           
-            total = sum(exp['сумма'] for exp in expenses)
-            total_amount.value = f"Общая сумма: {total} сом"
-
-            expense_name_input.value = ""
-            expense_amount_input.value = ""
+        if first_name:
+            new_friend = {"имя": first_name, "дополнительно": last_name_or_age}
+            friends.append(new_friend)
+            print(f"Добавлен друг: {new_friend}")
+            first_name_input.value = ""
+            last_name_or_age_input.value = ""
             page.update()
-        else:
-            page.show_snack_bar(
-                ft.SnackBar(
-                    ft.Text("Пожалуйста, введите название расхода и корректную сумму."),
-                    open=True,
-                )
-            )
 
-    save_button = ft.ElevatedButton(text="Добавить расход", on_click=add_expense)
+    add_button = ft.ElevatedButton(text="Добавить друга", on_click=add_friend)
 
     page.add(
-        expense_name_input,
-        expense_amount_input,
-        save_button,
-        ft.Divider(),
-        ft.Text("Список расходов:", weight=ft.FontWeight.BOLD),
-        expenses_list_view,
-        ft.Divider(),
-        total_amount,
+        first_name_input,
+        last_name_or_age_input,
+        add_button
     )
 
 if __name__ == "__main__":
     ft.app(target=main)
-    
